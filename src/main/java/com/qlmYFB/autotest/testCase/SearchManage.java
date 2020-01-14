61db6adb11a4722da7a8855c3cca0ad851cfd93b
liuye <1137962860@qq.com>
2020-01-14 13:44:28 +0800

    运行项目的一部分修改

--- src/main/java/com/qlmYFB/autotest/testCase/SearchManage.java
+++ src/main/java/com/qlmYFB/autotest/testCase/SearchManage.java
@@ -19,7 +19,8 @@ public class SearchManage {
     @Test
     public  void  BigSearch() throws  Exception{
         WaitTime.waitTime(5000);
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[2]/div/div[2]")).click();
+        driver.findElement(By.xpath(" //*[@id=\"app\"]/div[6]/div[2]/div/div[2]")).click();
+
         WaitTime.waitTime(2000);
         driver.findElement(By.cssSelector("input")).click();
         driver.findElement(By.cssSelector("input[type=\"search\"]")).clear();
@@ -27,39 +28,39 @@ public class SearchManage {
         WaitTime.waitTime(2000);
         driver.findElement(By.cssSelector("div.cancel-btn-text")).click();
         WaitTime.waitTime(2000);
-//        选择时间
-        driver.findElement(By.cssSelector("p.active > span")).click();
-        WaitTime.waitTime(2000);
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[3]/div/ul/li[3]")).click();
-        WaitTime.waitTime(8000);
-//        选择区域
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/ul/li[1]/p/span")).click();
-        WaitTime.waitTime(2000);
-        driver.findElement(By.xpath("//*[contains(text(),'北京')]")).click();
-        WebElement element =  driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[2]/div/div[1]/div/div[2]/ul/li/div[2]/img"));
-        if(element.getAttribute("src").contains("Fja4gAAAABJRU5ErkJggg==")){
-            System.out.print(element.getAttribute("src").contains("Fja4gAAAABJRU5ErkJggg=="));
-            MyClick.myClick(driver,element);
-        }
-        WaitTime.waitTime(2000);
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[2]/div/div[2]/div/button")).click();
-        WaitTime.waitTime(3000);
-
-
-//      选择信息类型
-
-        driver.findElement(By.xpath("//div[@id='app']/div[5]/div[3]/aside/ul/li[3]/p/span")).click();
-        WaitTime.waitTime(2000);
-        driver.findElement(By.xpath("//*[contains(text(),'拟建项目')]")).click();
-        WaitTime.waitTime(3000);
-//选择智能检索
-        driver.findElement(By.xpath("//div[@id='app']/div[5]/div[3]/aside/ul/li[4]/p/span")).click();
-        WaitTime.waitTime(2000);
-        driver.findElement(By.xpath("//*[contains(text(),'精准检索')]")).click();
-        WaitTime.waitTime(3000);
-        driver.findElement(By.xpath("//*[contains(text(),'标题检索')]")).click();
-        WaitTime.waitTime(3000);
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[5]/div/div/div/button")).click();
+////        选择时间
+//        driver.findElement(By.cssSelector("p.active > span")).click();
+//        WaitTime.waitTime(2000);
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[3]/div/ul/li[3]")).click();
+//        WaitTime.waitTime(8000);
+////        选择区域
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/ul/li[1]/p/span")).click();
+//        WaitTime.waitTime(2000);
+//        driver.findElement(By.xpath("//*[contains(text(),'北京')]")).click();
+//        WebElement element =  driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[2]/div/div[1]/div/div[2]/ul/li/div[2]/img"));
+//        if(element.getAttribute("src").contains("Fja4gAAAABJRU5ErkJggg==")){
+//            System.out.print(element.getAttribute("src").contains("Fja4gAAAABJRU5ErkJggg=="));
+//            MyClick.myClick(driver,element);
+//        }
+//        WaitTime.waitTime(2000);
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[2]/div/div[2]/div/button")).click();
+//        WaitTime.waitTime(3000);
+//
+//
+////      选择信息类型
+//
+//        driver.findElement(By.xpath("//div[@id='app']/div[5]/div[3]/aside/ul/li[3]/p/span")).click();
+//        WaitTime.waitTime(2000);
+//        driver.findElement(By.xpath("//*[contains(text(),'拟建项目')]")).click();
+//        WaitTime.waitTime(3000);
+////选择智能检索
+//        driver.findElement(By.xpath("//div[@id='app']/div[5]/div[3]/aside/ul/li[4]/p/span")).click();
+//        WaitTime.waitTime(2000);
+//        driver.findElement(By.xpath("//*[contains(text(),'精准检索')]")).click();
+//        WaitTime.waitTime(3000);
+//        driver.findElement(By.xpath("//*[contains(text(),'标题检索')]")).click();
+//        WaitTime.waitTime(3000);
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div[3]/aside/section[5]/div/div/div/button")).click();
 
     }
 }
