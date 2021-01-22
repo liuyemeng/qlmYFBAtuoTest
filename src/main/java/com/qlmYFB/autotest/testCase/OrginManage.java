0be563f8a5b2043d831b555d7e8b1f161f2f4843
liuye <1137962860@qq.com>
2021-01-22 10:29:54 +0800

    放开options.addArguments("--headless");

--- src/main/java/com/qlmYFB/autotest/testCase/OrginManage.java
+++ src/main/java/com/qlmYFB/autotest/testCase/OrginManage.java
@@ -26,9 +26,7 @@ public class OrginManage {
 //    ly谷歌驱动调用
         System.setProperty("webdriver.chrome.driver",ChromDriverPath);
         ChromeOptions options = new ChromeOptions();
-/*
         options.addArguments("--headless");//是否显示可视窗格1
-*/
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--no-sandbox");
 
